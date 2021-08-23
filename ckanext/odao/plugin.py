e020f91108ba737eb6a14303d7617c741f985663
mango7j <cr4ssroad@gmail.com>
2021-08-23 17:34:33 +0900

    add token encoding

--- ckanext/odao/plugin.py
+++ ckanext/odao/plugin.py
@@ -3,6 +3,11 @@ from flask import Blueprint, make_response, jsonify
 from ckan import plugins as p
 from ckan.lib.authenticator import UsernamePasswordAuthenticator
 from ckan.logic import NotAuthorized
+import ckan.lib.api_token as api_token
+from datetime import datetime
+import logging 
+
+log = logging.getLogger(__name__)
 
 toolkit = p.toolkit
 
@@ -18,11 +23,28 @@ def _login():
             raise NotAuthorized('wrong id or password')
         
         tokens = toolkit.get_action('api_token_list')({ u'user': user }, { u'user': user })
-        response = make_response(
-            { u'success': True, u'result': tokens }
-        )
-        response.headers[u'Content-Type'] = u'application/json'
-        return response
+
+        token = next((x for x in tokens if x[u'name'] == u'ODAO_API_TOKEN_APP_CLIENT'), None)
+
+        if token:
+            dt = datetime.strptime(token[u'created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            data = {
+                u'jti': token[u'id'],
+                u'iat': api_token.into_seconds(dt)
+            }
+            token = api_token.encode(data)
+
+            response = make_response(
+                { u'success': True, u'result': token }
+            )
+            response.headers[u'Content-Type'] = u'application/json'
+            return response
+
+        else:
+            response = make_response(
+                { u'success': True, u'result': None }
+            )
+            return 
     except NotAuthorized as e:
         return {
             'success': False,
